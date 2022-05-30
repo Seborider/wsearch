7c494b77c7cdf733516975b9919ce52f32d0a9bb
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-05-30 15:49:27 +0200

    ✨ Implement child to parent communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wsearch';
+  onTerm(term: string) {
+    console.log("here is the term");
+    
+  }
 }

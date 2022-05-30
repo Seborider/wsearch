9ac455eb19067b4bc55eda57fc014d5163cde45b
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-05-30 17:34:09 +0200

    🎨 Formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,8 @@ import { WikipediaService } from './wikipedia.service';
 })
 export class AppComponent {
   constructor(private wikipedia: WikipediaService) {
-
   }
+  
   onTerm(term: string) {
     const results = this.wikipedia.search(term)
     console.log(results);
